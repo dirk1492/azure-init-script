06e41d3e9dbe661497937b670fe45dc66cca9966
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-09-25 11:09:10 +0200

    first ci

---
+++ init_node.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get update && apt-get dist-upgrade -y 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list 
+apt-get update && apt-get install -y docker.io kubelet kubeadm
+
+kubeadm join --token $1 master01:6443
