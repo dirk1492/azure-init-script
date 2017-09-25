69b4fd018e80923b983b718c1838b38d75f1ef2a
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-09-25 11:18:38 +0200

    add sleep statement

--- init_node.sh
+++ init_node.sh
@@ -7,4 +7,6 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list 
 apt-get update && apt-get install -y docker.io kubelet kubeadm
 
+sleep 20
+
 kubeadm join --token $1 master01:6443
