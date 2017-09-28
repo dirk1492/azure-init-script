a28b22cfe15553f87a269f3693212d616c4570bc
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-09-28 15:55:08 +0200

    add master script

---
+++ init_master.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get update && apt-get dist-upgrade -y 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list 
+apt-get update && apt-get install -y docker.io kubelet kubeadm
+
+sleep 20
+
+kubeadm init --token $1 --apiserver-bind-port $2 --apiserver-cert-extra-sans $3
+ 
