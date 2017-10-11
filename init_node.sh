c1db0e563b3e8d1aa9400d91c831daf48317b279
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-11 13:13:44 +0200

    remove kubeadm join

--- init_node.sh
+++ init_node.sh
@@ -6,7 +6,3 @@ apt-get update && apt-get dist-upgrade -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list 
 apt-get update && apt-get install -y docker.io kubelet kubeadm
-
-sleep 20
-
-kubeadm join --token $1 master01:6443
