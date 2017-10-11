d775f6f219ea86c8b1551eec7996cc4a1fc4ca5d
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-11 14:10:46 +0200

    remove kubeadm init

--- init_master.sh
+++ init_master.sh
@@ -7,7 +7,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list 
 apt-get update && apt-get install -y docker.io kubelet kubeadm
 
-sleep 20
+#sleep 20
 
-kubeadm init --token $1 --apiserver-bind-port $2 --apiserver-cert-extra-sans $3
+#kubeadm init --token $1 --apiserver-bind-port $2 --apiserver-cert-extra-sans $3
  
