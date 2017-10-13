8f25a7783d3e5e51a31c0aa60c5b24ee61d5ec48
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-13 08:38:20 +0200

    add master and nodes to /etc/hosts

--- init_master.sh
+++ init_master.sh
@@ -10,6 +10,14 @@ apt-get update && apt-get install -y docker.io kubelet kubeadm
 systemctl stop kubelet
 rm -rf /var/lib/kubelet
 
+echo "10.0.2.10 master" >> /etc/hosts
+
+echo "10.0.2.101 node01" >> /etc/hosts
+echo "10.0.2.102 node02" >> /etc/hosts
+echo "10.0.2.103 node03" >> /etc/hosts
+echo "10.0.2.104 node04" >> /etc/hosts
+echo "10.0.2.105 node05" >> /etc/hosts
+
 #sleep 20
 
 #kubeadm init --token $1 --apiserver-bind-port $2 --apiserver-cert-extra-sans $3
