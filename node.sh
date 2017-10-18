18b8d9fb912d785354924901fc7b518aa3f394d8
Ubuntu <dil@node0.ihwuunrs44lehjax0i1mru4oxd.bx.internal.cloudapp.net>
2017-10-18 07:33:55 +0000

    add missing master

--- node.sh
+++ node.sh
@@ -5,6 +5,7 @@ TOKEN=$(cat /home/dil/token.txt)
 while [ "$(curl -sL -w "%{http_code}\\n" "https://master/api/v1" -o /dev/null --connect-timeout 1 --max-time 1 --insecure)" != "200" ] ; do
  sleep 5
 done
+ 
 
-kubeadm join --token $TOKEN  --discovery-token-unsafe-skip-ca-verification
+sudo kubeadm join --token $TOKEN  --discovery-token-unsafe-skip-ca-verification master:443
  
