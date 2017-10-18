63c544f60faae0abfa2b6167fdb965f752023966
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-18 15:26:34 +0200

    copy admin.config

--- master.sh
+++ master.sh
@@ -8,4 +8,7 @@ until docker info &>/dev/null ; do
 done
 
 kubeadm init --token $TOKEN --apiserver-bind-port 443 --apiserver-cert-extra-sans $IP --token-ttl 0
- 
+
+sudo -u dil mkdir -p /home/dil/.kube
+cp /etc/kubernetes/admin.conf /home/dil/.kube/config
+chown dil /home/dil/.kube/config 
