0bc553a72717d1894345a7be009efcc9201a3654
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-18 09:59:14 +0200

    wait for docker

--- master.sh
+++ master.sh
@@ -3,5 +3,9 @@
 IP=$(dig k8s-dil001.eastus.cloudapp.azure.com | grep "^k8s-dil001.eastus.cloudapp.azure.com" | awk '{ print $5 }')
 TOKEN=$(cat /tmp/token.txt)
 
+until docker info &>/dev/null ; do
+   sleep 1
+done
+
 kubeadm init --token $TOKEN --apiserver-bind-port 443 --apiserver-cert-extra-sans $IP
  
