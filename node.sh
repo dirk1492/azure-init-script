1c68ee45e83115c5f8d8aac05bff4af88ea65afa
Ubuntu <dil@master.ihwuunrs44lehjax0i1mru4oxd.bx.internal.cloudapp.net>
2017-10-18 07:27:46 +0000

    wait for master

--- node.sh
+++ node.sh
@@ -2,5 +2,9 @@
 
 TOKEN=$(cat /home/dil/token.txt)
 
-kubeadm join --token --discovery-token-unsafe-skip-ca-verification
+while [ "$(curl -sL -w "%{http_code}\\n" "https://master/api/v1" -o /dev/null --connect-timeout 1 --max-time 1 --insecure)" != "200" ] ; do
+ sleep 5
+done
+
+kubeadm join --token $TOKEN  --discovery-token-unsafe-skip-ca-verification
  
