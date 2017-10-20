05bc5c112c7d2b7cc45bd0713735e777a862cee4
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-20 12:27:48 +0200

    copy rook admin secret to kube-system namespace

--- deploy-registry.sh
+++ deploy-registry.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+
+kubectl get secret rook-admin -n rook -o json | jq '.metadata.namespace = "kube-system"' | kubectl apply -f -
+
 export MONS=$(kubectl -n rook get service -l app=rook-ceph-mon -o json|jq ".items[].spec.clusterIP"|tr -d "\""|sed -e 's/$/:6790/'|paste -s -d, -)
 sed "s/INSERT_MONS_HERE/$MONS/g" kube-registry.yaml | kubectl create -f -
