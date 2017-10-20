996fabe77d7f3d2d45c04238adab0ffada5bd6fe
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-20 07:07:58 +0200

    add content

---
+++ etcd.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+docker run -d \
+--restart always \
+-v /etc/ssl/certs:/etc/ssl/certs \
+-v /var/lib/etcd-cluster:/var/lib/etcd \
+-p 4001:4001 \
+-p 2380:2380 \
+-p 2379:2379 \
+--name etcd \
+gcr.io/google_containers/etcd-amd64:3.0.17 \
+etcd --name=etcd$1 \
+--advertise-client-urls=http://node$1:2379,http://node$1:4001 \
+--listen-client-urls=http://0.0.0.0:2379,http://0.0.0.0:4001 \
+--initial-advertise-peer-urls=http://node$1:2380 \
+--listen-peer-urls=http://0.0.0.0:2380 \
+--initial-cluster-token=9477af68bbee1b9ae037d6fd9e7efefd \
+--initial-cluster=etcd0=http://node0:2380,etcd1=http://node1:2380,etcd2=http://node2:2380 \
+--initial-cluster-state=new \
+--auto-tls \
+--peer-auto-tls \
+--data-dir=/var/lib/etcd
