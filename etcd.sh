ab74ec1ef1dfbc07bdf8c02d59f1188cf358f42b
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-10-20 07:42:01 +0200

    remove bugs

--- etcd.sh
+++ etcd.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 HOSTNAME=$(hostname)
-HOSTIP=$(ifconfig eth0 | grep "inet addr:" | cut -d':' -f 2 | cut -d' ' -f 1)
-
+HostIP=$(ifconfig eth0 | grep "inet addr:" | cut -d':' -f 2 | cut -d' ' -f 1)
 
 docker run -d \
 --restart always \
@@ -14,11 +13,11 @@ docker run -d \
 --name etcd \
 gcr.io/google_containers/etcd-amd64:3.0.17 \
 etcd --name=etcd${HOSTNAME:4} \
---advertise-client-urls=http://$HOSTNAME:2379,http://$HOSTNAME:4001 \
+--advertise-client-urls=http://${HostIP}:2379,http://${HostIP}:4001 \
 --listen-client-urls=http://0.0.0.0:2379,http://0.0.0.0:4001 \
---initial-advertise-peer-urls=http://$HOSTNAME:2380 \
+--initial-advertise-peer-urls=http://${HostIP}:2380 \
 --listen-peer-urls=http://0.0.0.0:2380 \
---initial-cluster-token=9477af68bbee1b9ae037d6fd9e7efefd \
+--initial-cluster-token=59563cd6-b558-11e7-8157-000d3a1c15d2 \
 --initial-cluster=etcd0=http://node0:2380,etcd1=http://node1:2380,etcd2=http://node2:2380 \
 --initial-cluster-state=new \
 --auto-tls \
