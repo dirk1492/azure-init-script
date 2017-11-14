326129fb408021701fabc133d5d18ef58c729120
Dirk Lembke <dirklembke@Dirks-MacBook-Pro.local>
2017-11-14 14:17:18 +0100

    remove message from wait loop

--- init.sh
+++ init.sh
@@ -5,19 +5,8 @@ export DEBIAN_FRONTEND="noninteractive"
 sleep 10
 
 # wait for dpkg 
-i=0
-tput sc
 while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-    case $(($i % 4)) in
-        0 ) j="-" ;;
-        1 ) j="\\" ;;
-        2 ) j="|" ;;
-        3 ) j="/" ;;
-    esac
-    tput rc
-    echo -en "\r[$j] Waiting for other software managers to finish..." 
     sleep 0.5
-    ((i=i+1))
 done 
 
 dpkg -l docker.io >/dev/null 2>&1
