210edd08dfccb75b11d8edf9d0ffcac7e7675dc8
Dirk Lembke <dil001@web.de>
2017-11-24 13:03:07 +0100

    use apt-get instead of apt

--- init.sh
+++ init.sh
@@ -18,8 +18,8 @@ if [ $? -ne 0 ];  then
     if [ ! -f "/etc/apt/sources.list.d/ansible-ubuntu-ansible-xenial.list" ]; then
       apt-add-repository -y ppa:ansible/ansible
     fi
-    apt update
-    apt install -y ansible
+    apt-get update
+    apt-get install -y ansible
   fi
 fi
 
